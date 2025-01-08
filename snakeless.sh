088f3ee59bcac860da5ef7327ad8e57eaeb9ba0b
yyx <yuxin.yan.cs@gmail.com>
2025-01-09 01:48:34 +0800

    fix: change the output directory of data

--- snakeless.sh
+++ snakeless.sh
@@ -22,8 +22,13 @@ echo -e "================== Debug Info ==================\n\n"
 
 basedir=$(pwd)
 datadir=/data/Competitions/ASC25/m5C/data
+dataout="${basedir}/data"
 num_threads=32
 srr=${srr:-SRR23538290}
+
+if [! -f ${dataout} ]; then
+    mkdir -p "data/${srr}"
+fi
 
 ## Only process one srr file.
 ## The following commands should be executed for each dataset.
@@ -35,15 +40,15 @@ cutseq "${datadir}/${srr}/${srr}.fastq" \
     --min-length 20 \
     --trim-polyA \
     --ensure-inline-barcode \
-    --output-file "${datadir}/${srr}/${srr}.fastq_cut" \
-    --short-file "${datadir}/${srr}/${srr}.fastq_tooshort" \
-    --untrimmed-file "${datadir}/${srr}/${srr}.fastq_untrimmed"
+    --output-file "${dataout}/${srr}/${srr}.fastq_cut" \
+    --short-file "${dataout}/${srr}/${srr}.fastq_tooshort" \
+    --untrimmed-file "${dataout}/${srr}/${srr}.fastq_untrimmed"
 
 echo "====== get ncrna.mapped.bam: $(date) ======"
 ./hisat-3n/hisat-3n --index "${datadir}/ncrna_ref/Homo_sapiens.GRCh38.ncrna.fa" \
-    --summary-file "${datadir}/${srr}/map2ncrna.output.summary" \
+    --summary-file "${dataout}/${srr}/map2ncrna.output.summary" \
     --new-summary -q \
-    -U "${datadir}/${srr}/${srr}.fastq_cut" \
+    -U "${dataout}/${srr}/${srr}.fastq_cut" \
     --threads "${num_threads}" \
     --base-change C,T \
     --mp 8,2 \
@@ -52,19 +57,19 @@ echo "====== get ncrna.mapped.bam: $(date) ======"
     ./samtools-1.21/samtools view --threads "${num_threads}" \
         --expr '!flag.unmap' \
         --output-fmt BAM \
-        --unoutput "${datadir}/${srr}/${srr}.ncrna.unmapped.bam" \
-        --output "${datadir}/${srr}/${srr}.ncrna.mapped.bam"
+        --unoutput "${dataout}/${srr}/${srr}.ncrna.unmapped.bam" \
+        --output "${dataout}/${srr}/${srr}.ncrna.mapped.bam"
 
 echo "====== get mRNA.fastq: $(date) ======"
 ./samtools-1.21/samtools fastq --threads "${num_threads}" \
-    -O "${datadir}/${srr}/${srr}.ncrna.unmapped.bam" >"${datadir}/${srr}/${srr}.mRNA.fastq"
+    -O "${dataout}/${srr}/${srr}.ncrna.unmapped.bam" >"${dataout}/${srr}/${srr}.mRNA.fastq"
 
 echo "====== get mRNA.genome.mapped.bam: $(date) ======"
 ./hisat-3n/hisat-3n --index "${datadir}/ref/Homo_sapiens.GRCh38.dna.primary_assembly.fa" \
     --threads "${num_threads}" \
-    --summary-file "${datadir}/${srr}/map2genome.output.summary" \
+    --summary-file "${dataout}/${srr}/map2genome.output.summary" \
     --new-summary -q \
-    -U "${datadir}/${srr}/${srr}.mRNA.fastq" \
+    -U "${dataout}/${srr}/${srr}.mRNA.fastq" \
     --directional-mapping \
     --base-change C,T \
     --pen-noncansplice 20 \
@@ -72,40 +77,40 @@ echo "====== get mRNA.genome.mapped.bam: $(date) ======"
     ./samtools-1.21/samtools view --threads "${num_threads}" \
         --expr '!flag.unmap' \
         --output-fmt BAM \
-        --unoutput "${datadir}/${srr}/${srr}.mRNA.genome.unmapped.bam" \
-        --output "${datadir}/${srr}/${srr}.mRNA.genome.mapped.bam"
+        --unoutput "${dataout}/${srr}/${srr}.mRNA.genome.unmapped.bam" \
+        --output "${dataout}/${srr}/${srr}.mRNA.genome.mapped.bam"
 
 echo "====== get mRNA.genome.mapped.sorted.bam: $(date) ======"
 ./samtools-1.21/samtools sort --threads "${num_threads}" \
     --write-index \
     --output-fmt BAM \
-    -o "${datadir}/${srr}/${srr}.mRNA.genome.mapped.sorted.bam" \
-    "${datadir}/${srr}/${srr}.mRNA.genome.mapped.bam"
+    -o "${dataout}/${srr}/${srr}.mRNA.genome.mapped.sorted.bam" \
+    "${dataout}/${srr}/${srr}.mRNA.genome.mapped.bam"
 
 echo "====== get mRNA.genome.mapped.sorted.bam.tsv: $(date) ======"
 ./samtools-1.21/samtools view --threads "${num_threads}" \
     --exclude-flags 3980 \
     --count \
-    "${datadir}/${srr}/${srr}.mRNA.genome.mapped.sorted.bam" >"${datadir}/${srr}/${srr}.mRNA.genome.mapped.sorted.bam.tsv"
+    "${dataout}/${srr}/${srr}.mRNA.genome.mapped.sorted.bam" >"${dataout}/${srr}/${srr}.mRNA.genome.mapped.sorted.bam.tsv"
 
 echo "====== get mRNA.genome.mapped.sorted.dedup.bam: $(date) ======"
-java -server -Xms8G -Xmx40G -Xss100M -Djava.io.tmpdir="${datadir}/${srr}" \
+java -server -Xms8G -Xmx40G -Xss100M -Djava.io.tmpdir="${dataout}/${srr}" \
     -jar ./UMICollapse-1.0.0/umicollapse.jar bam \
     -t 2 -T 16 \
     --data naive \
     --merge avgqual \
     --two-pass \
-    -i "${datadir}/${srr}/${srr}.mRNA.genome.mapped.sorted.bam" \
-    -o "${datadir}/${srr}/${srr}.mRNA.genome.mapped.sorted.dedup.bam" >"${datadir}/${srr}/${srr}.mRNA.genome.mapped.sorted.dedup.log"
+    -i "${dataout}/${srr}/${srr}.mRNA.genome.mapped.sorted.bam" \
+    -o "${dataout}/${srr}/${srr}.mRNA.genome.mapped.sorted.dedup.bam" >"${dataout}/${srr}/${srr}.mRNA.genome.mapped.sorted.dedup.log"
 
 echo "====== get mRNA.genome.mapped.sorted.dedup.bam.bai: $(date) ======"
 ./samtools-1.21/samtools index --threads "${num_threads}" \
-    "${datadir}/${srr}/${srr}.mRNA.genome.mapped.sorted.dedup.bam" \
-    "${datadir}/${srr}/${srr}.mRNA.genome.mapped.sorted.dedup.bam.bai"
+    "${dataout}/${srr}/${srr}.mRNA.genome.mapped.sorted.dedup.bam" \
+    "${dataout}/${srr}/${srr}.mRNA.genome.mapped.sorted.dedup.bam.bai"
 
 echo "====== get unifiltered_unique.tsv.gz: $(date) ======"
 ./samtools-1.21/samtools view --expr "rlen<100000" \
-    --with-header "${datadir}/${srr}/${srr}.mRNA.genome.mapped.sorted.dedup.bam" |
+    --with-header "${dataout}/${srr}/${srr}.mRNA.genome.mapped.sorted.dedup.bam" |
     ./hisat-3n/hisat-3n-table --threads "${num_threads}" \
         --unique-only \
         --alignments - \
@@ -113,11 +118,11 @@ echo "====== get unifiltered_unique.tsv.gz: $(date) ======"
         --output-name /dev/stdout \
         --base-change C,T |
     cut -f 1,2,3,5,7 |
-    gzip -c >"${datadir}/${srr}/${srr}_unfiltered_uniq.tsv.gz"
+    gzip -c >"${dataout}/${srr}/${srr}_unfiltered_uniq.tsv.gz"
 
 echo "====== get unfiltered_multi.tsv.gz: $(date) ======"
 ./samtools-1.21/samtools view --expr "rlen<100000" \
-    --with-header "${datadir}/${srr}/${srr}.mRNA.genome.mapped.sorted.dedup.bam" |
+    --with-header "${dataout}/${srr}/${srr}.mRNA.genome.mapped.sorted.dedup.bam" |
     ./hisat-3n/hisat-3n-table --threads "${num_threads}" \
         --multiple-only \
         --alignments - \
@@ -125,18 +130,18 @@ echo "====== get unfiltered_multi.tsv.gz: $(date) ======"
         --output-name /dev/stdout \
         --base-change C,T |
     cut -f 1,2,3,5,7 |
-    gzip -c >"${datadir}/${srr}/${srr}_unfiltered_multi.tsv.gz"
+    gzip -c >"${dataout}/${srr}/${srr}_unfiltered_multi.tsv.gz"
 
 echo "====== get mRNA.genome.mapped.sorted.dedup.filtered.bam: $(date) ======"
 ./samtools-1.21/samtools view --threads "${num_threads}" \
     --expr "[XM] * 20 <= (qlen-sclen) && [Zf] <= 3 && 3 * [Zf] <= [Zf] + [Yf]" \
-    "${datadir}/${srr}/${srr}.mRNA.genome.mapped.sorted.dedup.bam" \
+    "${dataout}/${srr}/${srr}.mRNA.genome.mapped.sorted.dedup.bam" \
     --output-fmt BAM \
-    --output "${datadir}/${srr}/${srr}.mRNA.genome.mapped.sorted.dedup.filtered.bam"
+    --output "${dataout}/${srr}/${srr}.mRNA.genome.mapped.sorted.dedup.filtered.bam"
 
 echo "====== get filtered_uniq.tsv.gz: $(date) ======"
 ./samtools-1.21/samtools view --expr "rlen<100000" \
-    --with-header "${datadir}/${srr}/${srr}.mRNA.genome.mapped.sorted.dedup.filtered.bam" |
+    --with-header "${dataout}/${srr}/${srr}.mRNA.genome.mapped.sorted.dedup.filtered.bam" |
     ./hisat-3n/hisat-3n-table --threads "${num_threads}" \
         --unique-only \
         --alignments - \
@@ -144,11 +149,11 @@ echo "====== get filtered_uniq.tsv.gz: $(date) ======"
         --output-name /dev/stdout \
         --base-change C,T |
     cut -f 1,2,3,5,7 |
-    gzip -c >"${datadir}/${srr}/${srr}_filtered_uniq.tsv.gz"
+    gzip -c >"${dataout}/${srr}/${srr}_filtered_uniq.tsv.gz"
 
 echo "====== get filtered_multi.tsv.gz: $(date) ======"
 ./samtools-1.21/samtools view --expr "rlen<100000" \
-    --with-header "${datadir}/${srr}/${srr}.mRNA.genome.mapped.sorted.dedup.filtered.bam" |
+    --with-header "${dataout}/${srr}/${srr}.mRNA.genome.mapped.sorted.dedup.filtered.bam" |
     ./hisat-3n/hisat-3n-table --threads "${num_threads}" \
         --multiple-only \
         --alignments - \
@@ -156,14 +161,14 @@ echo "====== get filtered_multi.tsv.gz: $(date) ======"
         --output-name /dev/stdout \
         --base-change C,T |
     cut -f 1,2,3,5,7 |
-    gzip -c >"${datadir}/${srr}/${srr}_filtered_multi.tsv.gz"
+    gzip -c >"${dataout}/${srr}/${srr}_filtered_multi.tsv.gz"
 
 echo "====== join_pileup and get genome.arror: $(date) ======"
 python ./m5C-UBSseq-0.1/bin/join_pileup.py -i \
-    "${datadir}/${srr}/${srr}_unfiltered_uniq.tsv.gz" \
-    "${datadir}/${srr}/${srr}_unfiltered_multi.tsv.gz" \
-    "${datadir}/${srr}/${srr}_filtered_uniq.tsv.gz" \
-    "${datadir}/${srr}/${srr}_filtered_multi.tsv.gz" \
-    -o "${datadir}/${srr}/${srr}_genome.arrow"
+    "${dataout}/${srr}/${srr}_unfiltered_uniq.tsv.gz" \
+    "${dataout}/${srr}/${srr}_unfiltered_multi.tsv.gz" \
+    "${dataout}/${srr}/${srr}_filtered_uniq.tsv.gz" \
+    "${dataout}/${srr}/${srr}_filtered_multi.tsv.gz" \
+    -o "${dataout}/${srr}/${srr}_genome.arrow"
 
 echo -e "\n\nDone: $(date)"
