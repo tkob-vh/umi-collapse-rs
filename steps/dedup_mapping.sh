262f17c5de73104b93710580d1d2a2cb1c4d0cdc
yyx <yuxin.yan.cs@gmail.com>
2025-01-26 19:26:54 +0800

    fix: fix the dedup_mapping script

--- steps/dedup_mapping.sh
+++ steps/dedup_mapping.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-java -server -Xms8G -Xmx40G -Xss100M -Djava.io.tmpdir="${dataout}/${srr}" \
-  -jar ./UMICollapse-1.0.0/umicollapse.jar bam \
-  -t 2 -T 16 \
+# java -server -Xms80G -Xmx150G -Xss100M -Djava.io.tmpdir="${dataout}/${srr}" \
+#   -jar ./UMICollapse-1.0.0/umicollapse.jar bam \
+#   --data naive \
+#   --merge avgqual \
+#   -i "${dataout}/${srr}/${srr}.mRNA.genome.mapped.sorted.bam" \
+#   -o "${dataout}/${srr}/${srr}.mRNA.genome.mapped.sorted.dedup.bam" >"${dataout}/${srr}/${srr}.mRNA.genome.mapped.sorted.dedup.log"
+./umicollapse/target/release/umicollapse --mode bam \
   --data naive \
   --merge avgqual \
-  --two-pass \
+  --num-threads 64 \
   -i "${dataout}/${srr}/${srr}.mRNA.genome.mapped.sorted.bam" \
-  -o "${dataout}/${srr}/${srr}.mRNA.genome.mapped.sorted.dedup.bam" >"${dataout}/${srr}/${srr}.mRNA.genome.mapped.sorted.dedup.log"
+  -o "${dataout}/${srr}/${srr}.mRNA.genome.mapped.sorted.dedup.bam"
