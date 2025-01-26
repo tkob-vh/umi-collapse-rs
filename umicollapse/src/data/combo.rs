232c50fa734c063493d7b707296dfa622c8be706
yyx <yuxin.yan.cs@gmail.com>
2025-01-26 19:28:47 +0800

    fix: fix the gitignore, track the data module

---
+++ umicollapse/src/data/combo.rs
@@ -0,0 +1,106 @@
+use std::collections::{HashMap, HashSet};
+use std::sync::Arc;
+
+use crate::utils::{bitset::BitSet, char_equals, char_set};
+
+use super::{Data, DataStruct};
+
+pub struct Combo {
+    umi_freq: HashMap<Arc<BitSet>, i32>,
+    umi_length: usize,
+}
+
+impl Combo {
+    #[allow(dead_code)]
+    pub fn new() -> Self {
+        Self {
+            umi_freq: HashMap::new(),
+            umi_length: 0,
+        }
+    }
+
+    fn recursive_remove_near(
+        &mut self,
+        umi: &BitSet,
+        idx: usize,
+        k: i32,
+        max_freq: i32,
+        curr: &mut BitSet,
+        res: &mut HashSet<Arc<BitSet>>,
+        kk: i32,
+    ) {
+        if k < 0 {
+            return;
+        }
+
+        if idx >= self.umi_length {
+            if self.umi_freq.contains_key(curr)
+                && (k == kk || self.umi_freq.get(curr).unwrap() <= &max_freq)
+            {
+                res.insert(Arc::new(curr.clone()));
+                self.umi_freq.remove(curr);
+            }
+            return;
+        }
+
+        for &c in crate::utils::read::ENCODING_IDX.keys() {
+            if char_equals(umi, idx, c) {
+                self.recursive_remove_near(
+                    umi,
+                    idx + 1,
+                    k,
+                    max_freq,
+                    char_set(curr, idx, c),
+                    res,
+                    kk,
+                );
+            } else {
+                self.recursive_remove_near(
+                    umi,
+                    idx + 1,
+                    k - 1,
+                    max_freq,
+                    char_set(curr, idx, c),
+                    res,
+                    kk,
+                );
+            }
+        }
+    }
+}
+
+impl Data for Combo {}
+
+impl DataStruct for Combo {
+    #[allow(unused_variables)]
+    fn change(&mut self, umi_freq: HashMap<Arc<BitSet>, i32>, umi_length: usize, max_edits: i32) {
+        self.umi_freq = umi_freq;
+        self.umi_length = umi_length;
+    }
+    fn remove_near(
+        &mut self,
+        umi: &BitSet,
+        k: i32,
+        max_freq: i32,
+    ) -> std::collections::HashSet<Arc<BitSet>> {
+        let mut res: HashSet<Arc<BitSet>> = HashSet::new();
+        self.recursive_remove_near(
+            umi,
+            0,
+            k,
+            max_freq,
+            &mut BitSet::new_with_len(self.umi_length * crate::utils::read::ENCODING_LENGTH),
+            &mut res,
+            k,
+        );
+        res
+    }
+
+    fn contains(&self, umi: &BitSet) -> bool {
+        self.umi_freq.contains_key(umi)
+    }
+
+    fn stats(&self) -> HashMap<String, f32> {
+        HashMap::new()
+    }
+}
---
+++ umicollapse/src/data/naive.rs
@@ -0,0 +1,75 @@
+//! `Naive` data structure.
+
+use std::collections::{HashMap, HashSet};
+use std::sync::Arc;
+
+use crate::utils::{bitset::BitSet, umi_dist};
+
+use super::{Data, DataStruct};
+
+pub struct Naive {
+    umi_freq: HashMap<Arc<BitSet>, i32>,
+}
+
+impl Naive {
+    pub fn new() -> Self {
+        Self {
+            umi_freq: HashMap::new(),
+        }
+    }
+}
+
+impl Data for Naive {}
+
+impl DataStruct for Naive {
+    #[allow(unused_variables)]
+    fn change(&mut self, umi_freq: HashMap<Arc<BitSet>, i32>, umi_length: usize, max_edits: i32) {
+        self.umi_freq = umi_freq;
+    }
+    fn remove_near(&mut self, umi: &BitSet, k: i32, max_freq: i32) -> HashSet<Arc<BitSet>> {
+        let mut res: HashSet<Arc<BitSet>> = HashSet::new();
+
+        // 创建一个要删除的key的列表
+        let to_remove: Vec<Arc<BitSet>> = self
+            .umi_freq
+            .iter()
+            .filter(|(o, &f)| {
+                let dist = umi_dist(umi, o);
+                dist <= k && (dist == 0 || f <= max_freq)
+            })
+            .map(|(o, _)| o.clone())
+            .collect();
+
+        // 从map中删除并添加到结果集
+        for key in to_remove {
+            self.umi_freq.remove(&key);
+            res.insert(key);
+        }
+
+        res
+    }
+
+    // fn remove_near(&mut self, umi: &BitSet, k: i32, max_freq: i32) -> HashSet<Arc<BitSet>> {
+    //     let mut res: HashSet<Arc<BitSet>> = HashSet::new();
+    //     let umi_freq = &mut self.umi_freq;
+    //     umi_freq.retain(|o, &mut f| {
+    //         let dist: i32 = umi_dist(umi, o);
+    //         if dist <= k && (dist == 0 || f <= max_freq) {
+    //             res.insert(o.clone());
+    //             false
+    //         } else {
+    //             true
+    //         }
+    //     });
+
+    //     res
+    // }
+
+    fn contains(&self, umi: &BitSet) -> bool {
+        self.umi_freq.contains_key(umi)
+    }
+
+    fn stats(&self) -> HashMap<String, f32> {
+        HashMap::new()
+    }
+}
