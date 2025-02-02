97d9d867a2093f209ec4e96ae66d2f145e09fa9f
Yuxin Yan <yuxin.yan.cs@gmail.com>
2025-02-02 21:05:37 +0800

    perf(umi): optimize remove_near

--- umicollapse/src/data/naive.rs
+++ umicollapse/src/data/naive.rs
@@ -24,44 +24,22 @@ impl DataStruct for Naive {
     fn change(&mut self, umi_freq: HashMap<Rc<BitSet>, i32>, umi_length: usize, max_edits: i32) {
         self.umi_freq = umi_freq;
     }
+
     fn remove_near(&mut self, umi: &BitSet, k: i32, max_freq: i32) -> HashSet<Rc<BitSet>> {
         let mut res: HashSet<Rc<BitSet>> = HashSet::new();
-
-        // 创建一个要删除的key的列表
-        let to_remove: Vec<Rc<BitSet>> = self
-            .umi_freq
-            .iter()
-            .filter(|(o, &f)| {
-                let dist = umi_dist(umi, o);
-                dist <= k && (dist == 0 || f <= max_freq)
-            })
-            .map(|(o, _)| o.clone())
-            .collect();
-
-        // 从map中删除并添加到结果集
-        for key in to_remove {
-            self.umi_freq.remove(&key);
-            res.insert(key);
-        }
+        let umi_freq = &mut self.umi_freq;
+        umi_freq.retain(|o, &mut f| {
+            let dist: i32 = umi_dist(umi, o);
+            if dist <= k && (dist == 0 || f <= max_freq) {
+                res.insert(o.clone());
+                false
+            } else {
+                true
+            }
+        });
 
         res
     }
-
-    // fn remove_near(&mut self, umi: &BitSet, k: i32, max_freq: i32) -> HashSet<Rc<BitSet>> {
-    //     let mut res: HashSet<Rc<BitSet>> = HashSet::new();
-    //     let umi_freq = &mut self.umi_freq;
-    //     umi_freq.retain(|o, &mut f| {
-    //         let dist: i32 = umi_dist(umi, o);
-    //         if dist <= k && (dist == 0 || f <= max_freq) {
-    //             res.insert(o.clone());
-    //             false
-    //         } else {
-    //             true
-    //         }
-    //     });
-
-    //     res
-    // }
 
     fn contains(&self, umi: &BitSet) -> bool {
         self.umi_freq.contains_key(umi)
