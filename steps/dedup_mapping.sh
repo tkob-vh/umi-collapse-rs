9434a29064f0706880980cabdc2b1228a340ab4d
Yuxin Yan <yuxin.yan.cs@gmail.com>
2025-03-19 20:45:50 +0800

    perf(umi): bind the processes manually

--- steps/dedup_mapping.sh
+++ steps/dedup_mapping.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-# java -server -Xms80G -Xmx150G -Xss100M -Djava.io.tmpdir="${dataout}/${srr}" \
-#   -jar ./UMICollapse-1.0.0/umicollapse.jar bam \
-#   --data naive \
-#   --merge avgqual \
-#   -i "${dataout}/${srr}/${srr}.mRNA.genome.mapped.sorted.bam" \
-#   -o "${dataout}/${srr}/${srr}.mRNA.genome.mapped.sorted.dedup.bam" >"${dataout}/${srr}/${srr}.mRNA.genome.mapped.sorted.dedup.log"
-./umicollapse/target/release/umicollapse --mode bam \
+numactl --cpunodebind=0 --membind=0 ./umicollapse/target/release/umicollapse --mode bam \
   --data naive \
   --merge avgqual \
   --num-threads 16 \
